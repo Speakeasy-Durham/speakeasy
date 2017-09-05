623fd49f9af682579328acefb6198513f1c26c0e
swallsy <smwall84@gmail.com>
2017-09-05 12:07:38 -0400

    adds user info to firebase database from facebook

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,14 +12,35 @@ import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
+import * as firebase from 'firebase';
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
-  render() {
+  checkForUser() {
+    let user = firebase.auth().currentUser;
+
+    if (user != null) {
+      user.providerData.forEach(function (profile) {
+        console.log("Sign-in provider: "+profile.providerId);
+        console.log("  Provider-specific UID: "+profile.uid);
+        console.log("  Name: "+profile.displayName);
+        console.log("  Email: "+profile.email);
+        console.log("  Photo URL: "+profile.photoURL);
+      });
+    }
+  }
+
     // storeHighScore('swallsy', 2000);
-    return (
+
+    render() {
+      this.checkForUser();
+      // Expo.SecureStore.getValueWithKeyAsync("fbId");
+      //       .then( result => console.log(result))
+      //       .catch( error => console.log(error));
+      return (
       <View style={styles.container}>
 
         <ScrollView
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -8,26 +8,7 @@ import {
 } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 
-    // import children
-    // import * as firebase from 'firebase';
-    //
-    // const firebaseConfig = {
-    //   apiKey: "AIzaSyAkbmuMGHpzdJYyeyhokfO02_oqn9EmlAk",
-    //   authDomain: "tin-can-durm.firebaseapp.com",
-    //   databaseURL: "https://tin-can-durm.firebaseio.com",
-    //   storageBucket: "tin-can-durm.appspot.com"
-    // };
-    //
-    // const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-
-
-
-
- // dismissed and go to the homescreen (navigation)
- // it will need to see if you are signed in
- // use async storage to figure out if signed in
- // use storage to add information about user
+import * as firebase from 'firebase';
 
  const resetStack = NavigationActions.reset({
    index: 0,
@@ -41,68 +22,59 @@ export default class SignupScreen extends React.Component {
     header: null
   }
 
-  // storeHighScore(userId, score) {
-  //   firebase.database().ref('users/' + userId).set({
-  //     highscore: score
-  //   });
+
+  // async logInGoogle(accessToken) {
+  //     try {
+  //       const result = await Expo.Google.logInAsync({
+  //         behavior: 'web',
+  //         androidClientId: "464757889372-7rot9u82fm6ncvpec4cl4kng4elo10mk.apps.googleusercontent.com",
+  //         iosClientId: "464757889372-0l11hjb6nmrstbfp56q437l17sq68tqu.apps.googleusercontent.com",
+  //         scopes: ['profile', 'email'],
+  //       });
+  //       if (result.type === 'success') {
+  //         return result.accessToken;
+  //       } else {
+  //         return {cancelled: true};
+  //       }
+  //     } catch(e) {
+  //       return {error: true};
+  //     }
   // }
-
-  async logInGoogle(accessToken) {
-      try {
-        const result = await Expo.Google.logInAsync({
-          behavior: 'web',
-          androidClientId: "464757889372-7rot9u82fm6ncvpec4cl4kng4elo10mk.apps.googleusercontent.com",
-          iosClientId: "464757889372-0l11hjb6nmrstbfp56q437l17sq68tqu.apps.googleusercontent.com",
-          scopes: ['profile', 'email'],
-        });
-        if (result.type === 'success') {
-          return result.accessToken;
-        } else {
-          return {cancelled: true};
-        }
-      } catch(e) {
-        return {error: true};
-      }
-  }
-
 
 
   async logInFB() {
     const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('113477732653798', {
-        permissions: ['public_profile'],
+        permissions: ['public_profile', 'email'],
       });
     if (type === 'success') {
-
       const response = await fetch(
-        `https://graph.facebook.com/me?access_token=${token}`);
-
+        `https://graph.facebook.com/me?fields=id,name,picture,email&access_token=${token}`);
       const fbInfo = await response.json();
-      console.log(fbInfo);
-
-      Expo.SecureStore.setValueWithKeyAsync(fbInfo.id, "fbId").then( () => {
-        Expo.SecureStore.getValueWithKeyAsync("fbId")
-          .then( result => console.log(result))
-          .catch( error => console.log(error));
-      });
-        // console.log(fbToken);
+      let access_token = token;
+      var credential = firebase.auth.FacebookAuthProvider.credential(access_token);
+      firebase.auth().signInWithCredential(credential).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+});
       Alert.alert(
         'Logged in!',
         `Hi ${fbInfo.name}!`,
       );
-
       this.props.navigation.dispatch(resetStack);
     }
   }
 
+
+
   _handleFacebookAuth = ()  => {
     this.logInFB();
   }
-_handleGoogleAuth = () => {
-  this.logInGoogle();
-}
 
-
-
+  // _handleGoogleAuth = () => {
+  //   this.logInGoogle();
+  // }
   render () {
     // storeHighSchore("swallsy", 4500);
     return (
@@ -111,12 +83,6 @@ _handleGoogleAuth = () => {
         <Button
           onPress={this._handleFacebookAuth}
           title="Facebook"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Button
-          onPress={this._handleGoogleAuth}
-          title="Google+"
           color="#841584"
           accessibilityLabel="Learn more about this purple button"
         />
