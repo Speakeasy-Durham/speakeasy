558b6f2c249c94ae6031824dca6f44a628c546a5
swallsy <smwall84@gmail.com>
2017-09-10 13:24:46 -0400

    Adds Google authentication, splash screen, and fixes log out

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -22,23 +22,25 @@ export default class SignupScreen extends React.Component {
     header: null
   }
 
-
-  async logInGoogle(accessToken) {
-      try {
-        const result = await Expo.Google.logInAsync({
-          behavior: 'web',
-          androidClientId: "464757889372-7rot9u82fm6ncvpec4cl4kng4elo10mk.apps.googleusercontent.com",
-          iosClientId: "464757889372-0l11hjb6nmrstbfp56q437l17sq68tqu.apps.googleusercontent.com",
-          scopes: ['profile', 'email'],
-        });
-        if (result.type === 'success') {
-          return result.accessToken;
-        } else {
-          return {cancelled: true};
-        }
-      } catch(e) {
-        return {error: true};
-      }
+  render () {
+    this.checkLoggedIn();
+    return (
+      <View style={styles.main}>
+        <Text style={styles.mainText}>This is the signup Screen</Text>
+        <Button
+          onPress={this._handleFacebookAuth}
+          title="Facebook"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress={this._handleGoogleAuth}
+          title="Google"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    )
   }
 
   checkLoggedIn() {
@@ -47,6 +49,45 @@ export default class SignupScreen extends React.Component {
         this.props.navigation.dispatch(resetStack);
       }
     });
+  }
+
+  async logInGoogle() {
+
+      try {
+        const result = await Expo.Google.logInAsync({
+          behavior: 'web',
+          iosClientId: "464757889372-ug65j7ujl3013g51l4jb1mj6rqdc77mi.apps.googleusercontent.com",
+          scopes: ['profile', 'email'],
+        });
+
+        if (result.type === 'success') {
+          var credential = firebase.auth.GoogleAuthProvider.credential(null, result.accessToken);
+          firebase.auth().signInWithCredential(credential).catch(function(error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+          });
+          firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+            .then(function() {
+              return firebase.auth().signInWithEmailAndPassword(email, password);
+            })
+            .catch(function(error) {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+            });
+          Alert.alert(
+            'Logged in!',
+          );
+          this.props.navigation.dispatch(resetStack);
+          return result.accessToken;
+        } else {
+          return {cancelled: true};
+        }
+      } catch(e) {
+        console.log(e);
+        return {error: true};
+      }
   }
 
   async logInFB() {
@@ -67,11 +108,6 @@ export default class SignupScreen extends React.Component {
       });
       firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
         .then(function() {
-    // Existing and future Auth states are now persisted in the current
-    // session only. Closing the window would clear any existing state even
-    // if a user forgets to sign out.
-    // ...
-    // New sign-in will be persisted with session persistence.
           return firebase.auth().signInWithEmailAndPassword(email, password);
         })
         .catch(function(error) {
@@ -92,26 +128,6 @@ export default class SignupScreen extends React.Component {
 
   _handleGoogleAuth = () => {
     this.logInGoogle();
-  }
-  render () {
-    this.checkLoggedIn();
-    return (
-      <View style={styles.main}>
-        <Text style={styles.mainText}>This is the signup Screen</Text>
-        <Button
-          onPress={this._handleFacebookAuth}
-          title="Facebook"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Button
-          onPress={this._handleGoogleAuth}
-          title="Google"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-    )
   }
 }
 
