5c3abcfb6867ba041e10f2499a2b245ee4403a7e
Michael Prechter <m.r.prechter@gmail.com>
2017-10-15 19:39:29 -0400

    minor changes

--- screens/RecordingScreen.js
+++ screens/RecordingScreen.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { Button, Icon } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationActions, StackNavigator } from 'react-navigation';
 import _NewRecording from '../components/new-recording/_NewRecording';
 
 
 export default class RecordingScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Record',
+    // headerRight: ({ goBack }) => <Icon name={"close"} onPress={goBack} />
+  }
+
   // static navigationOptions = {
-  //   title: 'Record',
-  //   header: null,
-  // }
+  //   title: "Record",
+  //   header: ({state, setParams, goBack}, defaultHeader) => ({
+  //       ...defaultHeader,
+  //       left:
+  //         <Button title="back" transparent onPress={e => {
+  //           e.preventDefault();
+  //           goBack();
+  //           }
+  //         }>
+  //           <Ionicons name="ios-arrow-round-back" style ={{color:"#fff", marginLeft:5}} />
+  //         </Button>
+  //     })
+  // };
 
   render() {
     return (
